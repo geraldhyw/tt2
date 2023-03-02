df4005cf7fba1b8f7a4e70d4a404678f7291cc2c
geraldhyw <gerald5jan@yahoo.com.sg>
2023-03-02 23:10:52 +0800

    added base code for login page

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../css/LoginPage.css'
 
 const LoginPage = () => {
+  const [EmployeeID, setEmployeeID] = useState('')
+  const [Password, setPassword] = useState('')
+
+  const handleLogin = (e) => {
+    e.preventDefault()
+    console.log(EmployeeID, Password)
+  }
+
   return (
-    <div>LoginPage</div>
+    <div className="login-container">
+      <div className="login-element">
+        <h2 className="login-header">
+          Welcome to DBSClaims
+        </h2>
+        <form onSubmit={(e) => handleLogin(e)}>
+          <div className="login-form-field">
+            <label>EmployeeID</label>
+            <input 
+              type="number"
+              value={EmployeeID}
+              onChange={(e) => setEmployeeID(e.target.value)}
+            />
+          </div>
+
+          <div className="login-form-field">
+            <label>Password</label>
+            <input 
+              type="password"
+              value={Password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
