45302efa9fa8a6de16cd6e5365aad93b785b4d8d
geraldhyw <gerald5jan@yahoo.com.sg>
2023-03-02 22:20:49 +0800

    added routing to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import CreateClaimPage from './pages/CreateClaimPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route 
+              path='/login'
+              element={<LoginPage />}
+            />
+            <Route 
+              path='/dashboard'
+              element={<DashboardPage />}
+            />
+            <Route 
+              path='/create-claim'
+              element={<CreateClaimPage />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ frontend/src/pages/CreateClaimPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateClaimPage = () => {
+  return (
+    <div>CreateClaimPage</div>
+  )
+}
+
+export default CreateClaimPage
