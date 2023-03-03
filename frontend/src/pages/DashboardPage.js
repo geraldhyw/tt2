428f10880e5a543659feea3aac6f0999211c45c8
geraldhyw <gerald5jan@yahoo.com.sg>
2023-03-03 23:54:27 +0800

    integrated GET all claims

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useEffect }  from 'react'
 import ClaimCard from '../components/ClaimCard'
 import '../css/DashboardPage.css'
 import { useClaimContext } from '../hooks/useClaimContext'
 
 const DashboardPage = () => {
   const {claims, dispatch} = useClaimContext()
+  const EmployeeID = 58001005
 
+  useEffect(() => {
+    const fetchClaims = async () => {
+      const response = await fetch('/api/claims/' + EmployeeID)
+      const json = await response.json()
+
+      if (response.ok) {
+        dispatch({ type: 'GET_CLAIMS', payload: json.claims })
+      }
+    }
+
+    fetchClaims()
+  }, [])
 
   return (
     <div className="dashboard-container">
