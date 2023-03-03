ee3a928cc25ef1c6d3a8b433caf0334cde556fd7
geraldhyw <gerald5jan@yahoo.com.sg>
2023-03-03 23:41:58 +0800

    added claim context for frontend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'material-symbols';
+import { ClaimContextProvider } from './contexts/ClaimContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ClaimContextProvider>
+      <App />
+    </ClaimContextProvider>
   </React.StrictMode>
 );
--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -1,50 +1,11 @@
 import React from 'react'
 import ClaimCard from '../components/ClaimCard'
 import '../css/DashboardPage.css'
+import { useClaimContext } from '../hooks/useClaimContext'
 
 const DashboardPage = () => {
+  const {claims, dispatch} = useClaimContext()
 
-  const claims = [
-    {
-     "ClaimID": 2010,
-     "InsuranceID": 1009,
-     "FirstName": "Martin",
-     "LastName": "Ong",
-     "ExpenseDate": "2022-07-14T08:00:00+08:00",
-     "Amount": 100.00,
-     "Purpose": "Dentist",
-     "FollowUp": 0,
-     "PreviousClaimID": null,
-     "Status": "Approved",
-     "LastEditedClaimDate": "2022-07-15T12:22:45+08:00"
-    },
-    {
-     "ClaimID": 2011,
-     "InsuranceID": 1008,
-     "FirstName": "John",
-     "LastName": "Tan",
-     "ExpenseDate": "2022-08-15T08:00:00+08:00",
-     "Amount": 100.00,
-     "Purpose": "Outpatient Claim",
-     "FollowUp": 0,
-     "PreviousClaimID": null,
-     "Status": "Rejected",
-     "LastEditedClaimDate": "2022-08-16T19:35:53+08:00"
-    },
-    {
-     "ClaimID": 2012,
-     "InsuranceID": 1005,
-     "FirstName": "Mary",
-     "LastName": "Lee",
-     "ExpenseDate": "2022-08-16T08:00:00+08:00",
-     "Amount": 200.00,
-     "Purpose": "Specialist Visit",
-     "FollowUp": 0,
-     "PreviousClaimID": null,
-     "Status": "Pending",
-     "LastEditedClaimDate": "2022-08-17T12:28:46+08:00"
-    }
-  ]
 
   return (
     <div className="dashboard-container">
